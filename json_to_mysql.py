e1e2a3c88c73f855586e87c5801360a979b82772
Neil Freeman <fitnr@fakeisthenewreal>
2013-05-31 19:27:06 -0400

    speed up, fix 2 errors

--- json_to_mysql.py
+++ json_to_mysql.py
@@ -3,65 +3,76 @@ import json
 import MySQLdb
 from datetime import datetime
 """
-one row looks like this:
+  one row looks like this:
 
-"id":72,
-"stationName":"W 52 St & 11 Av",
-"availableDocks":28,
-"totalDocks":39,
-"latitude":40.76727216,
-"longitude":-73.99392888,
-"statusValue":"In Service",
-"statusKey":1,
-"availableBikes":7,
-"stAddress1":"W 52 St & 11 Av",
-"stAddress2":"",
-"city":"","postalCode":"",
-"location":"","altitude":"",
-"testStation":false,
-"lastCommunicationTime":null,
-"landMark":""
+  "id":72,
+  "stationName":"W 52 St & 11 Av",
+  "availableDocks":28,
+  "totalDocks":39,
+  "latitude":40.76727216,
+  "longitude":-73.99392888,
+  "statusValue":"In Service",
+  "statusKey":1,
+  "availableBikes":7,
+  "stAddress1":"W 52 St & 11 Av",
+  "stAddress2":"",
+  "city":"","postalCode":"",
+  "location":"","altitude":"",
+  "testStation":false,
+  "lastCommunicationTime":null,
+  "landMark":""
 
-station_status looks like:
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `station_id` int(11) NOT NULL,
-  `availableDocks` smallint(5) unsigned DEFAULT NULL,
-  `totalDocks` smallint(5) unsigned DEFAULT NULL,
-  `statusValue` varchar(128) DEFAULT NULL,
-  `statusKey` tinyint(1) DEFAULT NULL,
-  `availableBikes` smallint(5) unsigned DEFAULT NULL,
-  `lastCommunicationTime` datetime DEFAULT NULL,
-  `stamp` datetime DEFAULT NULL,
+  station_status looks like:
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `station_id` int(11) NOT NULL,
+    `availableDocks` smallint(5) unsigned DEFAULT NULL,
+    `totalDocks` smallint(5) unsigned DEFAULT NULL,
+    `statusValue` varchar(128) DEFAULT NULL,
+    `statusKey` tinyint(1) DEFAULT NULL,
+    `availableBikes` smallint(5) unsigned DEFAULT NULL,
+    `lastCommunicationTime` datetime DEFAULT NULL,
+    `stamp` datetime DEFAULT NULL,
 
-stations looks like:
-  `id` int(10) unsigned NOT NULL,
-  `stationName` varchar(128) DEFAULT NULL,
-  `latitude` double(10,8) DEFAULT NULL,
-  `longitude` double(10,8) DEFAULT NULL,
-  `stAddress1` varchar(256) DEFAULT NULL,
-  `stAddress2` varchar(256) DEFAULT NULL,
-  `city` varchar(128) DEFAULT NULL,
-  `postalCode` varchar(128) DEFAULT NULL,
-  `location` varchar(128) DEFAULT NULL,
-  `altitude` int(11) DEFAULT NULL,
-  `landMark` varchar(128) DEFAULT NULL,
-  `communityboard` smallint(3) unsigned DEFAULT NULL,
+  stations looks like:
+    `id` int(10) unsigned NOT NULL,
+    `stationName` varchar(128) DEFAULT NULL,
+    `latitude` double(10,8) DEFAULT NULL,
+    `longitude` double(10,8) DEFAULT NULL,
+    `stAddress1` varchar(256) DEFAULT NULL,
+    `stAddress2` varchar(256) DEFAULT NULL,
+    `city` varchar(128) DEFAULT NULL,
+    `postalCode` varchar(128) DEFAULT NULL,
+    `location` varchar(128) DEFAULT NULL,
+    `altitude` int(11) DEFAULT NULL,
+    `landMark` varchar(128) DEFAULT NULL,
+    `communityboard` smallint(3) unsigned DEFAULT NULL,
 """
 
-INSERT_ROW = """INSERT INTO station_status (station_id, availableDocks, totalDocks, statusValue, statusKey, availableBikes, lastCommunicationTime, stamp) VALUES ({id}, {availableBikes}, {totalDocks}, '{statusValue}', {statusKey}, {availableBikes}, {lastCommunicationTime}, '{stamp}')"""
+INSERT_ROW = """INSERT INTO station_status (station_id, availableDocks, totalDocks, statusValue, statusKey, availableBikes, lastCommunicationTime, stamp) VALUES ({id}, {availableDocks}, {totalDocks}, '{statusValue}', {statusKey}, {availableBikes}, {lastCommunicationTime}, '{stamp}')"""
 
 INSERT_STATION = '''INSERT INTO stations (id, stationName, latitude, longitude, stAddress1, stAddress2, city, postalCode, location, altitude, landMark) VALUES ({id}, "{stationName}", {latitude}, {longitude}, "{stAddress1}", "{stAddress2}", '{city}', '{postalCode}', '{location}', {altitude}, '{landMark}')'''
 
 
 def json_to_mysql(f):
-    handle = open(f, 'rb')
-    data = json.load(handle)
+    try:
+        handle = open(f, 'rb')
+        data = json.load(handle)
+    except Exception as e:
+        print e, f
+        return
 
     stats = data['stationBeanList']
-    timestamp = datetime.strptime(data['executionTime'], '%Y-%m-%d %H:%M:%S %p').strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = datetime.strptime(data['executionTime'], '%Y-%m-%d %I:%M:%S %p').strftime('%Y-%m-%d %H:%M:%S')
 
     conn = MySQLdb.connect(host='localhost', user='root', passwd="mysqlpass", db="wp")
     cursor = conn.cursor()
+
+    # Check for previous insertion!
+    cursor.execute("SELECT COUNT(ID) FROM station_status WHERE stamp='{0}'".format(timestamp))
+    if cursor.fetchall()[0][0] > 0:
+
+        print 'skipping', timestamp, f
+        return
 
     # get list of all station_ids
     try:
@@ -72,12 +83,6 @@ def json_to_mysql(f):
     result = cursor.fetchall()
     station_ids = set(x[0] for x in result)
     # print 'found stations:', len(station_ids)
-
-    # Check for previous insertion!
-    cursor.execute("SELECT COUNT(ID) FROM station_status WHERE stamp='{0}'".format(timestamp))
-    if cursor.fetchall()[0][0] > 0:
-        print 'skipping', timestamp
-        return
 
     for row in stats:
 
