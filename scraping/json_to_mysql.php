ecb05984bd4e021e889aaa8d578acf63a4192a61
Neil Freeman <fitnr@fakeisthenewreal>
2013-06-03 14:34:19 -0400

    bugfixes for PDO statmts

--- scraping/json_to_mysql.php
+++ scraping/json_to_mysql.php
@@ -46,10 +46,35 @@
 */
 
 function json_to_mysql($f, $host, $user, $pword, $database) {
-  
+
   /* constants */
-  $INSERT_ROW = 'INSERT INTO station_status (station_id, availableDocks, totalDocks, statusValue, statusKey, availableBikes, lastCommunicationTime, stamp) VALUES (:id, :availableDocks, :totalDocks, ":statusValue", :statusKey, :availableBikes, :lastCommunicationTime, ":stamp")';
-  $INSERT_STATION = 'INSERT INTO stations (id, stationName, latitude, longitude, stAddress1, stAddress2, city, postalCode, location, altitude, landMark) VALUES (:id, ":stationName", :latitude, :longitude, ":stAddress1", ":stAddress2", ":city", ":postalCode", ":location", :altitude, ":landMark")';
+  $INSERT_ROW = 'INSERT INTO station_status (station_id, availableDocks, totalDocks, statusValue, statusKey, availableBikes, lastCommunicationTime, stamp) VALUES (:id, :availableDocks, :totalDocks, :statusValue, :statusKey, :availableBikes, :lastCommunicationTime, :stamp)';
+  $INSERT_STATION = 'INSERT INTO stations (id, stationName, latitude, longitude, stAddress1, stAddress2, city, postalCode, location, altitude, landMark) VALUES (:id, :stationName, :latitude, :longitude, :stAddress1, :stAddress2, :city, :postalCode, :location, :altitude, :landMark)';
+
+  $insert_row_keys = array(
+    "id" => NULL,
+    "availableDocks" => NULL,
+    "totalDocks" => NULL,
+    "statusValue" => NULL,
+    "statusKey" => NULL,
+    "availableBikes" => NULL,
+    "lastCommunicationTime" => NULL,
+    "stamp" => NULL
+  );
+
+  $insert_station_keys = array(
+    "id" => NULL,
+    "stationName" => NULL,
+    "latitude" => NULL,
+    "longitude" => NULL,
+    "stAddress1" => NULL,
+    "stAddress2" => NULL,
+    "city" => NULL,
+    "postalCode" => NULL,
+    "location" => NULL,
+    "altitude" => NULL,
+    "landMark" => NULL
+  );
 
   /// Open the JSON.
   try {
@@ -82,7 +107,7 @@ function json_to_mysql($f, $host, $user, $pword, $database) {
     $select_count->execute();
     $select_count->setFetchMode(PDO::FETCH_ASSOC);
     $result_count = $select_count->fetch();
-    
+
   } catch (PDOException $e) {
     echo $e->getMessage() ."\n";
   }
@@ -96,31 +121,39 @@ function json_to_mysql($f, $host, $user, $pword, $database) {
   try {
     $select_stn = $pdo->prepare("SELECT id FROM stations");
     $select_stn->execute();
-    $result_stations = $select_stn->fetch();
+    $select_stn->setFetchMode(PDO::FETCH_ASSOC);
+    $result_stations = $select_stn->fetchAll();
   } catch (PDOException $e) {
     print $e->getMessage() ."\n";
   }
 
   $station_ids = array();
   foreach ($result_stations as $stn):
-    $station_ids[] = $stn[0];
+    $station_ids[] = $stn['id'];
   endforeach;
   // echo 'found stations: '. count($station_ids);
 
   // Loop through JSON, formatting data a bit and inserting.
   foreach ($stats as $row):
+    $row = (array) $row;
     $row['lastCommunicationTime'] = ($row['lastCommunicationTime'] === '') ? 'NULL' : $row['lastCommunicationTime'];
     $row['stamp'] = $timestamp;
-
     // $row['stationName'] = row['stationName'].encode('ascii', 'ignore')
     // $row['stAddress1'] = row['stAddress1'].encode('ascii', 'ignore')
 
+    $row_data = array_intersect_key($row, $insert_row_keys);
+
     try {
-      $insert_row = $pdo->prepare($INSERT_ROW, $row);
-      $insert_row->execute();
+      $insert_row = $pdo->prepare($INSERT_ROW);
+      $insert_row->execute($row_data);
     } catch (PDOException $e) {
-      echo $insert_row->queryString ."\n";
       echo $e->getMessage() ."\n";
+      echo $insert_row->queryString;
+      echo $insert_row->debugDumpParams();
+      echo 'row data' . count($row_data) . "\n";
+      // var_dump($row_data);
+      echo 'insert_row_keys ' .count($insert_row_keys) . "\n";
+      // var_dump($insert_row_keys);
     }
     // echo sprintf("Auto Increment ID: %s", $pdo->lastInsertId);
 
@@ -128,14 +161,20 @@ function json_to_mysql($f, $host, $user, $pword, $database) {
     if (!in_array($row['id'], $station_ids)):
       // If not, fiddle with the data
       $row['altitude'] = ($row['altitude'] === '' || $row['altitude'] === NULL) ? '' : $row['altitude'];
-      
+
+      $station_data = array_intersect_key($row, $insert_station_keys);
       try {
-        $insert_stn = $pdo->prepare($INSERT_ROW, $row);
-        $insert_stn->execute();
+        $insert_stn = $pdo->prepare($INSERT_ROW);
+        $insert_stn->execute($row);
         // echo 'inserted station '. $row['id'];
       } catch (PDOException $e) {
-        echo 'problem inserting station '. $row['id'] ."\n";
         echo $e->getMessage() . "\n";
+        echo 'problem inserting station '. $station_data['id'] ."\n";
+        echo $insert_stn->queryString;
+        // echo $insert_stn->debugDumpParams();
+        echo 'station data ' . count($station_data) . "\n";
+        echo 'station ' .count($insert_station_keys) . "\n";
+        
       }
     endif;
 
