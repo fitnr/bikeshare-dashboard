e14169d836d9a8ed4460ca74b267a0ef808f8a7e
Neil Freeman <fitnr@fakeisthenewreal>
2013-06-05 13:57:05 -0400

    fix broken front page with since queries

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
  * @subpackage bikeshare-dashboard
  */
 
-// silence is golden
-
+// Wordpress drops you here if a custom query var is included with a designated front page.
+// Doesn't make much sense, but the solution is to just include the desired template
+if (isset($wp_query->query_vars['since'])):
+	get_template_part('dashboard', 'bikeshare');
+else:
+	// Otherwise, just give an empty page.
+	get_header('dashboard');
+	get_footer('dashboard');
+endif;
 ?>
